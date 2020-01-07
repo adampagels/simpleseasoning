b4ce43648950a82c517ab3de467519453443ed7f
Adam Pagels <pagels@hotmail.ca>
2020-01-06 23:06:57 -0500

    Add post method for user registration

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,10 +1,28 @@
 const express = require('express');
 const router = express.Router();
 
+const User = require('../models/User');
+
 // Login Page
 router.get('/login', (req, res) => res.send('Login'));
 
 // Register Page
 router.get('/register',  (req, res) => res.send('Register'));
+
+// Register User
+router.post('/register', async (req, res) => {
+    const { username, password, email } = req.body;
+    const user = new User({
+        username: username,
+        password: password,
+        email: email
+    });
+    try {
+        const savedUser = await user.save();
+        res.send(savedUser);
+    } catch (err) {
+        res.status(400).send(err);
+    }
+});
 
 module.exports = router;
