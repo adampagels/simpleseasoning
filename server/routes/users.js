004b61b978c12c2258e14a39a94576efea029e66
Adam Pagels <pagels@hotmail.ca>
2020-02-01 11:05:59 -0500

    Add conditional to test if email is already registered

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,28 +1,36 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-
-const User = require('../models/User');
+const User = require("../models/User");
+const { registerValidation } = require("../validation");
 
 // Login Page
-router.get('/login', (req, res) => res.send('Login'));
+router.get("/login", (req, res) => res.send("Login"));
 
 // Register Page
-router.get('/register',  (req, res) => res.send('Register'));
+router.get("/register", (req, res) => res.send("Register"));
 
 // Register User
-router.post('/register', async (req, res) => {
-    const { username, password, email } = req.body;
-    const user = new User({
-        username: username,
-        password: password,
-        email: email
-    });
-    try {
-        const savedUser = await user.save();
-        res.send(savedUser);
-    } catch (err) {
-        res.status(400).send(err);
-    }
+router.post("/register", async (req, res) => {
+	const { username, password, email } = req.body;
+
+	const { error } = registerValidation(req.body);
+	if (error) return res.status(400).send(error.details[0].message);
+
+	// Check if user is already registered
+	const emailExist = await User.findOne({ email: req.body.email });
+	if (emailExist) return res.status(400).send("Email already exists");
+
+	const user = new User({
+		username: username,
+		password: password,
+		email: email
+	});
+	try {
+		const savedUser = await user.save();
+		res.send(savedUser);
+	} catch (err) {
+		res.status(400).send(err);
+	}
 });
 
 module.exports = router;
