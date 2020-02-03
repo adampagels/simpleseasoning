24aea2e18ed513a51aecb0d245248ef2bdbb4fc6
Adam Pagels <pagels@hotmail.ca>
2020-02-02 19:42:59 -0500

    Add cors and routes for jobs

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
+
 
 const app = express();
 
@@ -11,11 +13,14 @@ mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
     .catch(err => console.log(err))
 
 app.use(express.json());
+app.use(cors());
 
 // Routes
 app.use('/', require('./routes/index'));
 app.use('/users', require('./routes/users'))
+app.use('/jobs', require('./routes/jobs'))
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
+    
