d7537bb5a92f709986fa93e2acd18c2840e2e664
Adam Pagels <pagels@hotmail.ca>
2020-02-02 23:30:30 -0500

    Allow user to register through axios post request

--- client/src/components/Registration.js
+++ client/src/components/Registration.js
@@ -1,1 +1,52 @@
+import React, { useState } from "react";
+import axios from "axios";
 
+export default function Registration() {
+	const [username, setUsername] = useState("");
+	const [password, setPassword] = useState("");
+	const [email, setEmail] = useState("");
+
+	const handleSubmit = event => {
+		event.preventDefault();
+		// console.log({username: username, password: password, email: email})
+		axios
+			.post("http://localhost:5000/users/register", {
+				username: username,
+				password: password,
+				email: email
+			})
+			.then(response => {
+				const data = response.data;
+				console.log(data);
+			})
+			.catch(error => {
+				console.log(error.response);
+			});
+	};
+
+	return (
+		<div>
+			<form>
+				<label>Username:</label>
+				<input
+					type="text"
+					value={username}
+					onChange={event => setUsername(event.target.value)}
+				></input>
+				<label>Password:</label>
+				<input
+					type="password"
+					value={password}
+					onChange={event => setPassword(event.target.value)}
+				></input>
+				<label>Email:</label>
+				<input
+					type="email"
+					value={email}
+					onChange={event => setEmail(event.target.value)}
+				></input>
+				<button onClick={handleSubmit}>Register</button>
+			</form>
+		</div>
+	);
+}
