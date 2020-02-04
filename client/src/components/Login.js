e0aa8056572a4e36130e948887e4244d0594c1f9
Adam Pagels <pagels@hotmail.ca>
2020-02-03 20:26:57 -0500

    Allow user to login through axios post request

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,14 +1,43 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
-class Login extends Component {
-	constructor() {
-		super()
-		this.state = {}
-	}
+export default function Login() {
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
 
-	render() {
-		return <div></div>;
-	}
+	const handleSubmit = event => {
+		event.preventDefault();
+		axios
+			.post("http://localhost:5000/users/login", {
+				email: email,
+				password: password
+			})
+			.then(response => {
+				const data = response.data;
+				localStorage.setItem("auth-token", data);
+			})
+			.catch(error => {
+				console.log(error.response);
+			});
+	};
+
+	return (
+		<div>
+			<form>
+				<label>Email:</label>
+				<input
+					type="email"
+					value={email}
+					onChange={event => setEmail(event.target.value)}
+				></input>
+				<label>Password:</label>
+				<input
+					type="password"
+					value={password}
+					onChange={event => setPassword(event.target.value)}
+				></input>
+				<button onClick={handleSubmit}>Login</button>
+			</form>
+		</div>
+	);
 }
-
-export default Login;
