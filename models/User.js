cbfe82324c2ed68df6ee3e20fa11ac68f6bf3deb
Adam Pagels <pagels@hotmail.ca>
2020-02-03 20:47:04 -0500

    Add reference to JobSchema

--- models/User.js
+++ models/User.js
@@ -1,20 +1,29 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
-    username: {
-        type: String,
-        required: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true
-    }
+	username: {
+		type: String,
+		required: true,
+		min: 4
+	},
+	password: {
+		type: String,
+		required: true,
+		min: 6
+	},
+	email: {
+		type: String,
+		required: true,
+		min: 6
+	},
+	jobs: [
+		{
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "Job"
+		}
+	]
 });
 
-const User = mongoose.model('User', UserSchema);
+const User = mongoose.model("User", UserSchema);
 
 module.exports = User;
