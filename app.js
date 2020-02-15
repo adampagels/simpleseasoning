38f11481a993c1a4d727880b708d5f9bae4e4de5
Adam Pagels <pagels@hotmail.ca>
2020-02-15 09:28:55 -0500

    Add body-parser

--- app.js
+++ app.js
@@ -1,26 +1,26 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-
-
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 const app = express();
+const bodyParser = require("body-parser");
 
 // DB Config
-const db = require('./config/config').MongoURI;
+const db = require("./config/config").MongoURI;
 
-mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('MongoDB Connected...'))
-    .catch(err => console.log(err))
+mongoose
+	.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+	.then(() => console.log("MongoDB Connected..."))
+	.catch(err => console.log(err));
 
 app.use(express.json());
 app.use(cors());
+app.use(bodyParser.json());
 
 // Routes
-app.use('/', require('./routes/index'));
-app.use('/users', require('./routes/users'))
-app.use('/jobs', require('./routes/jobs'))
+app.use("/", require("./routes/index"));
+app.use("/users", require("./routes/users"));
+app.use("/jobs", require("./routes/jobs"));
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
-    
