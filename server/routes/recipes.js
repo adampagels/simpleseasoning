b41f5a67abf0a8d364c77e0535289a7444406499
adampagels <pagels@hotmail.ca>
2020-08-20 23:02:59 -0400

    Add route for displaying single recipe

--- server/routes/recipes.js
+++ server/routes/recipes.js
@@ -4,6 +4,7 @@ const Recipe = require("../models/Recipe");
 const User = require("../models/User");
 const verify = require("./verifyToken");
 
+// Add recipe
 router.post("/", verify, async (req, res) => {
   const {
     title,
@@ -42,6 +43,7 @@ router.post("/", verify, async (req, res) => {
   );
 });
 
+// Get all recipes
 router.get("/", verify, async (req, res) => {
   try {
     const recipes = await Recipe.find();
@@ -50,6 +52,20 @@ router.get("/", verify, async (req, res) => {
     console.error(err);
     res.status(500).send("Request error: " + err);
   }
+});
+
+// Get single recipe
+router.get("/:RecipeID", verify, async (req, res) => {
+  Recipe.findOne({
+    _id: req.params.RecipeID,
+  })
+    .then((singleRecipe) => {
+      res.status(201).json(singleRecipe);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Request error: " + err);
+    });
 });
 
 module.exports = router;
