50ad6e39df88c010af9f538e2d2014d57c12f609
adampagels <pagels@hotmail.ca>
2020-08-24 22:55:13 -0400

    Test that password and email inputs properly change values

--- client/src/__tests__/AccountForm.test.js
+++ client/src/__tests__/AccountForm.test.js
@@ -12,11 +12,23 @@ describe("<AccountForm /> component", () => {
     shallow(<AccountForm />);
   });
 
-  it("should render name input", () => {
+  it("should render email input", () => {
     expect(AccountFormWrapper.find(".email")).toHaveLength(1);
   });
 
   it("should render password input", () => {
     expect(AccountFormWrapper.find(".password")).toHaveLength(1);
   });
+
+  it('should set the email value prop', () => {
+    const eventObject = { target: { value: 'email@email.com' }};
+    AccountFormWrapper.find('.email').simulate('change', eventObject);
+    expect(AccountFormWrapper.find('.email').prop('value')).toEqual('email@email.com');
+  });
+
+  it('should set the password value prop', () => {
+  const eventObject = { target: { value: 'password123' }};
+  AccountFormWrapper.find('.password').simulate('change', eventObject);
+  expect(AccountFormWrapper.find('.password').prop('value')).toEqual('password123');
+});
 });
