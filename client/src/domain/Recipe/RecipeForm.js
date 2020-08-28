8c9768de5e38649f1a43585a8d4dce769437b51c
adampagels <pagels@hotmail.ca>
2020-08-27 23:12:21 -0400

    Add form with input fields

---
+++ client/src/domain/Recipe/RecipeForm.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+const RecipeForm = () => {
+  return (
+    <form>
+      <label>
+        Title:
+        <input type="text" name="title" className="title-input" />
+      </label>
+      <label>
+        Description:
+        <input type="text" name="description" className="description-input" />
+      </label>
+      <label>
+        Ingredients:
+        <input type="text" name="ingredients" className="ingredients-input" />
+      </label>
+      <label>
+        Instructions:
+        <input type="text" name="instructions" className="instructions-input" />
+      </label>
+      <label>
+        Cook-Time:
+        <input type="text" name="cook-time" className="cook-time-input" />
+      </label>
+      <label>
+        Prep-Time:
+        <input type="text" name="prep-time" className="prep-time-input" />
+      </label>
+      <label>
+        Diet:
+        <input type="text" name="diet" className="diet-input" />
+      </label>
+      <button>Submit</button>
+    </form>
+  );
+};
+
+export default RecipeForm;
