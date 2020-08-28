3f41873f6a6bb8bb4ea2faa32348f175db23d7bf
adampagels <pagels@hotmail.ca>
2020-08-28 16:45:42 -0400

    Test if AccountForm renders on Login component

--- client/src/__tests__/Login.test.js
+++ client/src/__tests__/Login.test.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { shallow } from "enzyme";
 import Login from "../domain/Login/Login";
+import AccountForm from "../components/AccountForm/AccountForm";
 
 let LoginWrapper;
 beforeAll(() => {
@@ -10,5 +11,9 @@ beforeAll(() => {
 describe("<Login /> component", () => {
   it("renders", () => {
     shallow(<Login />);
+  });
+
+  it("should render AccountForm", () => {
+    expect(LoginWrapper.find(AccountForm)).toHaveLength(1);
   });
 });
