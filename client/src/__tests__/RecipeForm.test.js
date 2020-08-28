0e2dc6f0c27225f24f7582a13b201c83701afdd4
adampagels <pagels@hotmail.ca>
2020-08-27 22:55:46 -0400

    Test that all inputs exist

--- client/src/__tests__/RecipeForm.test.js
+++ client/src/__tests__/RecipeForm.test.js
@@ -11,4 +11,32 @@ describe("<RecipeFormWrapper /> component", () => {
   it("renders", () => {
     shallow(<RecipeForm />);
   });
+
+  it("should render title input", () => {
+    expect(RecipeFormWrapper.find(".title-input")).toHaveLength(1);
+  });
+
+  it("should render description input", () => {
+    expect(RecipeFormWrapper.find(".description-input")).toHaveLength(1);
+  });
+
+  it("should render ingredients input", () => {
+    expect(RecipeFormWrapper.find(".ingredients-input")).toHaveLength(1);
+  });
+
+  it("should render instructions input", () => {
+    expect(RecipeFormWrapper.find(".instructions-input")).toHaveLength(1);
+  });
+
+  it("should render cook-time input", () => {
+    expect(RecipeFormWrapper.find(".cook-time-input")).toHaveLength(1);
+  });
+
+  it("should render prep-time input", () => {
+    expect(RecipeFormWrapper.find(".prep-time-input")).toHaveLength(1);
+  });
+
+  it("should render diet input", () => {
+    expect(RecipeFormWrapper.find(".diet-input")).toHaveLength(1);
+  });
 });
