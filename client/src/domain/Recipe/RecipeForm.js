99a191f5050f4079f4a2cba33a2e5190b6f519ae
adampagels <pagels@hotmail.ca>
2020-08-30 16:36:57 -0400

    Add state and onChange method for saving input field data

--- client/src/domain/Recipe/RecipeForm.js
+++ client/src/domain/Recipe/RecipeForm.js
@@ -1,11 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
 const RecipeForm = () => {
+  const [values, setValues] = useState({
+    title: "",
+    image: "",
+    description: "",
+    ingredients: "",
+    instructions: "",
+    cookTime: "",
+    prepTime: "",
+    diet: "",
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setValues({ ...values, [name]: value });
+  };
+
   return (
     <form>
       <label>
         Title:
-        <input type="text" name="title" className="title-input" />
+        <input
+          type="text"
+          name="title"
+          className="title-input"
+          onChange={handleInputChange}
+          value={values.title}
+        />
       </label>
       <label>
         Image Upload:
@@ -13,27 +35,63 @@ const RecipeForm = () => {
       </label>
       <label>
         Description:
-        <input type="text" name="description" className="description-input" />
+        <input
+          type="text"
+          name="description"
+          className="description-input"
+          onChange={handleInputChange}
+          value={values.description}
+        />
       </label>
       <label>
         Ingredients:
-        <input type="text" name="ingredients" className="ingredients-input" />
+        <input
+          type="text"
+          name="ingredients"
+          className="ingredients-input"
+          onChange={handleInputChange}
+          value={values.ingredients}
+        />
       </label>
       <label>
         Instructions:
-        <input type="text" name="instructions" className="instructions-input" />
+        <input
+          type="text"
+          name="instructions"
+          className="instructions-input"
+          onChange={handleInputChange}
+          value={values.instructions}
+        />
       </label>
       <label>
         Cook-Time:
-        <input type="text" name="cook-time" className="cook-time-input" />
+        <input
+          type="text"
+          name="cookTime"
+          className="cook-time-input"
+          onChange={handleInputChange}
+          value={values.cookTime}
+        />
       </label>
       <label>
         Prep-Time:
-        <input type="text" name="prep-time" className="prep-time-input" />
+        <input
+          type="text"
+          name="prepTime"
+          className="prep-time-input"
+          onChange={handleInputChange}
+          value={values.prepTime}
+        />
       </label>
       <label>
         Diet:
-        <input type="text" name="diet" className="diet-input" />
+        <input
+          type="text"
+          name="diet"
+          className="diet-input"
+          onChange={handleInputChange}
+          value={values.diet}
+        />
       </label>
       <button>Submit</button>
     </form>
