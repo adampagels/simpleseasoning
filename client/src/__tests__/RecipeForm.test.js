287e6736cd0f522f8d7546f93fb469d4aac382f8
adampagels <pagels@hotmail.ca>
2020-08-29 23:51:24 -0400

    Test that all inputs properly change values

--- client/src/__tests__/RecipeForm.test.js
+++ client/src/__tests__/RecipeForm.test.js
@@ -43,4 +43,73 @@ describe("<RecipeFormWrapper /> component", () => {
   it("should render image input", () => {
     expect(RecipeFormWrapper.find(".image-input")).toHaveLength(1);
   });
+
+  it("should set the title value prop", () => {
+    const eventObject = { target: { name: "title", value: "recipe title" } };
+    RecipeFormWrapper.find(".title-input").simulate("change", eventObject);
+    expect(RecipeFormWrapper.find(".title-input").prop("value")).toEqual(
+      "recipe title"
+    );
+  });
+
+  it("should set the description value prop", () => {
+    const eventObject = {
+      target: { name: "description", value: "description of recipe" },
+    };
+    RecipeFormWrapper.find(".description-input").simulate(
+      "change",
+      eventObject
+    );
+    expect(RecipeFormWrapper.find(".description-input").prop("value")).toEqual(
+      "description of recipe"
+    );
+  });
+
+  it("should set the ingredients value prop", () => {
+    const eventObject = {
+      target: { name: "ingredients", value: "list of ingredients" },
+    };
+    RecipeFormWrapper.find(".ingredients-input").simulate(
+      "change",
+      eventObject
+    );
+    expect(RecipeFormWrapper.find(".ingredients-input").prop("value")).toEqual(
+      "list of ingredients"
+    );
+  });
+
+  it("should set the instructions value prop", () => {
+    const eventObject = {
+      target: { name: "instructions", value: "instructions for recipe" },
+    };
+    RecipeFormWrapper.find(".instructions-input").simulate(
+      "change",
+      eventObject
+    );
+    expect(RecipeFormWrapper.find(".instructions-input").prop("value")).toEqual(
+      "instructions for recipe"
+    );
+  });
+
+  it("should set the cook-time value prop", () => {
+    const eventObject = { target: { name: "cookTime", value: "cook time" } };
+    RecipeFormWrapper.find(".cook-time-input").simulate("change", eventObject);
+    expect(RecipeFormWrapper.find(".cook-time-input").prop("value")).toEqual(
+      "cook time"
+    );
+  });
+
+  it("should set the prep-time value prop", () => {
+    const eventObject = { target: { name: "prepTime", value: "prep time" } };
+    RecipeFormWrapper.find(".prep-time-input").simulate("change", eventObject);
+    expect(RecipeFormWrapper.find(".prep-time-input").prop("value")).toEqual(
+      "prep time"
+    );
+  });
+
+  it("should set the diet value prop", () => {
+    const eventObject = { target: { name: "diet", value: "diet" } };
+    RecipeFormWrapper.find(".diet-input").simulate("change", eventObject);
+    expect(RecipeFormWrapper.find(".diet-input").prop("value")).toEqual("diet");
+  });
 });
