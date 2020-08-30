2fdee41d513bbac57257e2e7e2156a3bb298bf4f
adampagels <pagels@hotmail.ca>
2020-08-29 20:02:56 -0400

    Test if AccountForm renders

--- client/src/__tests__/Register.test.js
+++ client/src/__tests__/Register.test.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { shallow } from "enzyme";
 import Register from "../domain/Register/Register";
+import AccountForm from "../components/AccountForm/AccountForm";
 
 let RegisterWrapper;
 beforeAll(() => {
@@ -10,5 +11,9 @@ beforeAll(() => {
 describe("<Register /> component", () => {
   it("renders", () => {
     shallow(<Register />);
+  });
+
+  it("should render AccountForm", () => {
+    expect(RegisterWrapper.find(AccountForm)).toHaveLength(1);
   });
 });
