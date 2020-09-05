859310788a806a0dea5ff9af7c1ed7936819437f
adampagels <pagels@hotmail.ca>
2020-09-05 12:14:18 -0400

    Add route for allowing a user to edit their rating of a recipe

--- routes/rating.js
+++ routes/rating.js
@@ -2,13 +2,16 @@ const express = require("express");
 const router = express.Router();
 const Recipe = require("../models/Recipe");
 const Rating = require("../models/Rating");
+const User = require("../models/User");
 const verify = require("./verifyToken");
 
+// Post a rating of a recipe
 router.post("/:RecipeID", verify, async (req, res) => {
   const { stars } = req.body;
 
   const rating = new Rating({
     stars: stars,
+    recipe: req.params.RecipeID,
     user: req.user,
   });
   try {
@@ -22,9 +25,37 @@ router.post("/:RecipeID", verify, async (req, res) => {
     { $push: { ratings: rating._id } },
     function (err, data) {
       if (data) {
-        console.log(data);
+        User.findByIdAndUpdate(
+          { _id: req.user._id },
+          { $push: { ratings: req.params.RecipeID } },
+          function (err, data) {
+            if (data) {
+              console.log(data);
+            } else {
+              console.log(err);
+            }
+          }
+        );
       } else {
         console.log(err);
+      }
+    }
+  );
+});
+
+// Edit rating of a recipe
+router.put("/:RecipeID", verify, async (req, res) => {
+  const { stars } = req.body;
+
+  Rating.findOne({ recipe: req.params.RecipeID, user: req.user._id }).exec(
+    function (err, rating) {
+      if (err) throw err;
+      if (rating) {
+        rating.stars = stars;
+        rating.save();
+        res.json(rating);
+      } else {
+        res.status(400).send(err);
       }
     }
   );
