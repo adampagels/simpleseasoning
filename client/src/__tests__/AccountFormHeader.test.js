e2e81563360581c88b7d121a7baa921834aef9db
adampagels <pagels@hotmail.ca>
2020-09-09 23:12:55 -0400

    Test that accountformheader-h1 class exists

--- client/src/__tests__/AccountFormHeader.test.js
+++ client/src/__tests__/AccountFormHeader.test.js
@@ -4,11 +4,17 @@ import AccountFormHeader from "../components/AccountFormHeader/AccountFormHeader
 
 let AccountFormHeaderWrapper;
 beforeAll(() => {
-  AccountFormHeaderWrapper = shallow(<AccountFormHeader />);
+  AccountFormHeaderWrapper = shallow(<AccountFormHeader page={"register"} />);
 });
 
 describe("<AccountFormHeaderWrapper /> component", () => {
   it("renders", () => {
     shallow(<AccountFormHeader />);
+  });
+
+  it("should render accountformheader-h1", () => {
+    expect(AccountFormHeaderWrapper.find(".accountformheader-h1")).toHaveLength(
+      1
+    );
   });
 });
