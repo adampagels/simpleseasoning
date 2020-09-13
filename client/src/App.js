dc7bcb4ba91ac0fa2302d4a47c1a80eb19b43edc
adampagels <pagels@hotmail.ca>
2020-09-13 15:56:59 -0400

    Add route for home

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Login from "../src/domain/Login/Login";
 import Register from "../src/domain/Register/Register";
+import Home from "../src/domain/Home/Home";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 const App = () => {
@@ -8,7 +9,8 @@ const App = () => {
     <Router>
       <div>
         <Switch>
-          <Route path="/login" component={Login} />
+          <Route path="/" exact component={Home} />
+          <Route path="/login" exact component={Login} />
           <Route path="/register" exact component={Register} />
         </Switch>
       </div>
