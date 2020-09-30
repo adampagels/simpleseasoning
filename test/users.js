9b98f6d3f30533e3113e59fed026d7ae884481ae
Adam Pagels <pagels@hotmail.ca>
2020-09-30 10:55:27 -0400

    Test if email is already registered

--- test/users.js
+++ test/users.js
@@ -6,6 +6,16 @@ let should = chai.should();
 chai.use(chaiHttp);
 
 describe("test user routes", () => {
+  it('should return status 400 when email is already registered', (done) => {
+    chai.request(app)
+      .post('/users/register')
+      .send({ username: "brandNewUser", email: "newUser@new.com", password: "newUser" })
+      .end((err, res) => {
+        res.should.have.status(400);
+        done();
+      });
+  });
+
   let token;
   before((done) => {
     chai.request(app)
@@ -18,16 +28,13 @@ describe("test user routes", () => {
       });
   })
 
-  describe('/GET requests', () => {
-    it('should return current user info', (done) => {
-      chai.request(app)
-        .get('/users/test')
-        .set({ "auth-token": `${token}` })
-        .end((err, res) => {
-          res.should.have.status(201);
-          done();
-        });
-    });
+  it('should return current user info', (done) => {
+    chai.request(app)
+      .get('/users/test')
+      .set({ "auth-token": `${token}` })
+      .end((err, res) => {
+        res.should.have.status(201);
+        done();
+      });
   });
 });
-  
