1cd06bfebd08f44acb989a21c21ec3e052a25f6a
Adam Pagels <pagels@hotmail.ca>
2020-09-30 15:59:10 -0400

    Test route to add recipe to favorite recipe list

--- test/users.js
+++ test/users.js
@@ -1,15 +1,20 @@
-let chai = require('chai');
-let chaiHttp = require('chai-http');
-let app = require('../app');
+let chai = require("chai");
+let chaiHttp = require("chai-http");
+let app = require("../app");
 let should = chai.should();
 
 chai.use(chaiHttp);
 
 describe("test user routes", () => {
-  it('should return status 400 when email is already registered', (done) => {
-    chai.request(app)
-      .post('/users/register')
-      .send({ username: "brandNewUser", email: "newUser@new.com", password: "newUser" })
+  it("should return status 400 when email is already registered", (done) => {
+    chai
+      .request(app)
+      .post("/users/register")
+      .send({
+        username: "brandNewUser",
+        email: "newUser@new.com",
+        password: "newUser",
+      })
       .end((err, res) => {
         res.should.have.status(400);
         done();
@@ -18,22 +23,36 @@ describe("test user routes", () => {
 
   let token;
   before((done) => {
-    chai.request(app)
-      .post('/users/login')
+    chai
+      .request(app)
+      .post("/users/login")
       .send({ email: "test@test.com", password: "testtest" })
       .end((err, res) => {
         res.should.have.status(200);
-        token = res.text
+        token = res.text;
         done();
       });
-  })
+  });
 
-  it('should return current user info', (done) => {
-    chai.request(app)
-      .get('/users/test')
+  it("should return current user info", (done) => {
+    chai
+      .request(app)
+      .get("/users/test")
       .set({ "auth-token": `${token}` })
       .end((err, res) => {
         res.should.have.status(201);
+        done();
+      });
+  });
+
+  it("should add recipe to user's favorite recipe list", (done) => {
+    chai
+      .request(app)
+      .post("/users/test/favorite-recipes/5f6405901b57d1085fe85d53")
+      .set({ "auth-token": `${token}` })
+      .end((err, res) => {
+        res.should.have.status(200);
+        console.log(res.text);
         done();
       });
   });
