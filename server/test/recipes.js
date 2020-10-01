bbd02bc34eb3ace22da662eab6ad9168800de270
Adam Pagels <pagels@hotmail.ca>
2020-10-01 13:35:13 -0400

    Test routes for adding and deleting a recipe

--- server/test/recipes.js
+++ server/test/recipes.js
@@ -1,6 +1,6 @@
-let chai = require('chai');
-let chaiHttp = require('chai-http');
-let app = require('../app');
+let chai = require("chai");
+let chaiHttp = require("chai-http");
+let app = require("../app");
 let should = chai.should();
 
 chai.use(chaiHttp);
@@ -8,20 +8,22 @@ chai.use(chaiHttp);
 describe("test recipe routes", () => {
   let token;
   before((done) => {
-    chai.request(app)
-      .post('/users/login')
+    chai
+      .request(app)
+      .post("/users/login")
       .send({ email: "test@test.com", password: "testtest" })
       .end((err, res) => {
         res.should.have.status(200);
-        token = res.text
+        token = res.text;
         done();
       });
-  })
+  });
 
-  describe('/GET requests', () => {
-    it('should return most recent recipes', (done) => {
-      chai.request(app)
-        .get('/recipes/most-recent')
+  describe("/GET requests", () => {
+    it("should return most recent recipes", (done) => {
+      chai
+        .request(app)
+        .get("/recipes/most-recent")
         .set({ "auth-token": `${token}` })
         .end((err, res) => {
           res.should.have.status(200);
@@ -29,13 +31,47 @@ describe("test recipe routes", () => {
         });
     });
 
-    it('should return a single recipe', (done) => {
-      chai.request(app)
-        .get('/recipes/5f6405901b57d1085fe85d53')
+    it("should return a single recipe", (done) => {
+      chai
+        .request(app)
+        .get("/recipes/5f6405901b57d1085fe85d53")
         .set({ "auth-token": `${token}` })
         .end((err, res) => {
           res.should.have.status(201);
-          console.log(res.text)
+          done();
+        });
+    });
+
+    let recipeId;
+    it("should add a recipe", (done) => {
+      chai
+        .request(app)
+        .post("/recipes/")
+        .set({ "auth-token": `${token}` })
+        .send({
+          title: "Big meal",
+          description: "testing food",
+          prepTime: 30,
+          cookTime: 20,
+          photo: "string of photo",
+          diet: ["high protein"],
+          ingredients: "food",
+          instructions: "Cook well",
+        })
+        .end((err, res) => {
+          res.should.have.status(200);
+          recipeId = res.text.replace(/"/g, "");
+          done();
+        });
+    });
+
+    it("should delete a recipe", (done) => {
+      chai
+        .request(app)
+        .delete(`/recipes/test/${recipeId}`)
+        .set({ "auth-token": `${token}` })
+        .end((err, res) => {
+          res.should.have.status(200);
           done();
         });
     });
