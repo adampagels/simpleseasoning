d8da5b562368b2793d866daf364d0c0a4ccfbd8e
Adam Pagels <pagels@hotmail.ca>
2020-10-23 17:53:14 -0400

    Get user info from user ID

---
+++ client/src/domain/Profile/Profile.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import RecipeCard from "../../components/RecipeCard/RecipeCard";
+import { withRouter } from "react-router-dom";
+
+const Profile = ({ location }) => {
+  const [user, setUser] = useState("");
+  const getUser = () => {
+    const accessToken = localStorage.getItem("auth-token");
+    axios
+      .get(`http://localhost:5000/users/${location.state.user}`, {
+        headers: {
+          "auth-token": `${accessToken}`,
+        },
+      })
+      .then((userData) => {
+        setUser(userData.data);
+      });
+  };
+
+  useEffect(() => {
+    getUser();
+  }, []);
+
+  return (
+    <>
+      <h1>Profile</h1>
+      <h2>{user.username}</h2>
+    </>
+  );
+};
+
+export default withRouter(Profile);
