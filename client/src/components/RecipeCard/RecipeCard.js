cfda21c6238586b657b0c57c73a6ab478e2ad382
Adam Pagels <pagels@hotmail.ca>
2020-10-23 16:43:51 -0400

    Add onClick prop to recipe creator name

--- client/src/components/RecipeCard/RecipeCard.js
+++ client/src/components/RecipeCard/RecipeCard.js
@@ -1,18 +1,20 @@
 import React from "react";
 
-const RecipeCard = ({ recentRecipes }) => {
+const RecipeCard = ({ recentRecipes, onClick }) => {
   return (
     <div className="recipecard-container">
       {recentRecipes &&
-        recentRecipes.map((recipes) => (
-          <div className="recipecard-wrapper">
+        recentRecipes.map((recipes, id) => (
+          <div className="recipecard-wrapper" key={recipes._id}>
             <img
               alt={recipes.title}
               className="recipecard-image"
               src={recipes.photo}
             />
             <h2>{recipes.title}</h2>
-            <h3>{recipes.creator.username}</h3>
+            <h3 onClick={() => onClick(recipes.creator._id)}>
+              {recipes.creator.username}
+            </h3>
             <p>{recipes.description}</p>
             <p>
               {recipes.ratings.reduce((x, y) => x + y.stars, 0) /
