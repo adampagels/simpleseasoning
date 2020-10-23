2b92777e5b9c41ff0b1e69ef216b1325187eb99d
Adam Pagels <pagels@hotmail.ca>
2020-10-23 11:50:24 -0400

    Add and description and creators username to recipe card

--- client/src/components/RecipeCard/RecipeCard.js
+++ client/src/components/RecipeCard/RecipeCard.js
@@ -2,16 +2,27 @@ import React from "react";
 
 const RecipeCard = ({ recentRecipes }) => {
   return (
-    <>
+    <div className="recipecard-container">
       {recentRecipes &&
         recentRecipes.map((recipes) => (
-          <>
-          {console.log(recipes)}
-        <img src={recipes.photo} />
-        <h1>{recipes.title}</h1>
-        </>
+          <div className="recipecard-wrapper">
+            <img
+              alt={recipes.title}
+              className="recipecard-image"
+              src={recipes.photo}
+            />
+            <h2>{recipes.title}</h2>
+            <h3>{recipes.creator.username}</h3>
+            <p>{recipes.description}</p>
+            <p>
+              {recipes.ratings.reduce((x, y) => x + y.stars, 0) /
+                recipes.ratings.length +
+                " " +
+                `(${recipes.ratings.length})`}
+            </p>
+          </div>
         ))}
-    </>
+    </div>
   );
 };
 
