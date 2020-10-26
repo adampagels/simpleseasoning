961a376fc2fece620a5ec6c941557337b5d6e4ad
Adam Pagels <pagels@hotmail.ca>
2020-10-26 19:10:29 -0400

    Add links to home and add-recipe pages

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -1,13 +1,18 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <div className="navbar-container">
       <ul className="navbar-item-list">
-        <li className="navbar-item">Home</li>
+        <NavLink to="/">
+          <li className="navbar-item">Home</li>
+        </NavLink>
         <input type="text" placeholder="Find recipes"></input>
         <button type="submit">Search</button>
-        <li className="navbar-item">Add Recipe</li>
+        <NavLink to="/add-recipe">
+          <li className="navbar-item">Add Recipe</li>
+        </NavLink>
         <li className="navbar-item">My Account</li>
       </ul>
     </div>
