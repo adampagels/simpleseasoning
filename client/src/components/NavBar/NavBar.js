7e15b3011903fca39a25b5889e62f17ee0eb73ea
Adam Pagels <pagels@hotmail.ca>
2020-10-30 12:14:26 -0400

    Get userId and pass as prop to Profile component

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { NavLink } from "react-router-dom";
+import axios from "axios";
 
 const NavBar = () => {
+  const [user, setUser] = useState("");
 
   const logOut = () => {
     localStorage.removeItem("auth-token");
-  }
+  };
+
+  const getUserId = () => {
+    const accessToken = localStorage.getItem("auth-token");
+    axios
+      .get(`http://localhost:5000/users/`, {
+        headers: {
+          "auth-token": `${accessToken}`,
+        },
+      })
+      .then((userData) => {
+        setUser(userData.data._id);
+      });
+  };
+
+  useEffect(() => {
+    getUserId();
+  }, []);
+
   return (
     <div className="navbar-container">
       <ul className="navbar-item-list">
@@ -17,8 +37,12 @@ const NavBar = () => {
         <NavLink to="/add-recipe">
           <li className="navbar-item">Add Recipe</li>
         </NavLink>
-        <li className="navbar-item">My Account</li>
-        <li className="navbar-item" onClick={() => logOut()}>Logout</li>
+        <NavLink to={{ pathname: `/user/${user}`, userId: user }}>
+          <li className="navbar-item">View Profile</li>
+        </NavLink>
+        <li className="navbar-item" onClick={() => logOut()}>
+          Logout
+        </li>
       </ul>
     </div>
   );
