ef2d485b3f92a64fd635af426ab7b93dd80b3332
Adam Pagels <pagels@hotmail.ca>
2020-10-29 21:03:01 -0400

    Show NavBar when on any screen other than login and register

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,18 @@
 import React from "react";
-import Login from "../src/domain/Login/Login";
-import Register from "../src/domain/Register/Register";
-import Home from "../src/domain/Home/Home";
-import Profile from "../src/domain/Profile/Profile";
-import Recipe from "../src/domain/Recipe/Recipe";
-import RecipeForm from "../src/domain/Recipe/RecipeForm";
-import NavBar from "../src/components/NavBar/NavBar";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Routes from "./Routes";
 
-const App = () => {
+import NavBar from "../src/components/NavBar/NavBar";
+import { withRouter } from "react-router-dom";
+
+const App = ({ location }) => {
   return (
-    <Router>
-      <div>
+    <div>
+      {location.pathname !== "/login" && location.pathname !== "/register" && (
         <NavBar />
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/login" exact component={Login} />
-          <Route path="/register" exact component={Register} />
-          <Route path="/user/:userid" exact component={Profile} />
-          <Route path="/recipe/:recipeid" exact component={Recipe} />
-          <Route path="/add-recipe" exact component={RecipeForm} />
-        </Switch>
-      </div>
-    </Router>
+      )}
+      <Routes />
+    </div>
   );
 };
 
-export default App;
+export default withRouter(App);
