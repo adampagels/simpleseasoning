1645304fe5e08e75c055bf1c81e82a6105fba707
Adam Pagels <pagels@hotmail.ca>
2020-10-29 20:31:54 -0400

    Add routes

---
+++ client/src/Routes.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Login from "../src/domain/Login/Login";
+import Register from "../src/domain/Register/Register";
+import Home from "../src/domain/Home/Home";
+import Profile from "../src/domain/Profile/Profile";
+import Recipe from "../src/domain/Recipe/Recipe";
+import RecipeForm from "../src/domain/Recipe/RecipeForm";
+import { BrowserRouter as Switch, Route } from "react-router-dom";
+
+const Routes = () => {
+  return (
+    <div>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/login" exact component={Login} />
+        <Route path="/register" exact component={Register} />
+        <Route path="/user/:userid" exact component={Profile} />
+        <Route path="/recipe/:recipeid" exact component={Recipe} />
+        <Route path="/add-recipe" exact component={RecipeForm} />
+      </Switch>
+    </div>
+  );
+};
+
+export default Routes;
