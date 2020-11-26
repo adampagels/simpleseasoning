4f53cdadc9c630d9b6686d41c107b29524559ec8
Adam Pagels <pagels@hotmail.ca>
2020-11-26 16:55:10 -0500

    Set input value with useState hook

--- client/src/components/SearchBar/SearchBar.js
+++ client/src/components/SearchBar/SearchBar.js
@@ -1,17 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 const SearchBar = () => {
+  const [searchValue, setSearchValue] = useState("");
+
   return (
-    <>
+    <div className="searchbar-container">
       <input
         type="text"
         placeholder="Find recipes"
         class="searchbar-input"
+        value={searchValue}
+        onChange={(event) => setSearchValue(event.target.value)}
       ></input>
       <button type="submit" class="searchbar-button">
         Search
       </button>
-    </>
+    </div>
   );
 };
 
