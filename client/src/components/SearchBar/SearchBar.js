391e8948fc17ffa0ff8813417c0ba9f3b6d4aff4
Adam Pagels <pagels@hotmail.ca>
2020-11-27 14:41:39 -0500

    Add function to pass inputValue and push to searchResults component

--- client/src/components/SearchBar/SearchBar.js
+++ client/src/components/SearchBar/SearchBar.js
@@ -1,22 +1,50 @@
 import React, { useState } from "react";
+import axios from "axios";
+import { withRouter } from "react-router-dom";
 
-const SearchBar = () => {
-  const [searchValue, setSearchValue] = useState("");
+const SearchBar = ({ history }) => {
+  const [inputValue, setInputValue] = useState("");
+
+  const accessToken = localStorage.getItem("auth-token");
+
+  const handleSearch = (searchValue) => {
+    return axios
+      .get(`http://localhost:5000/recipes/search/${searchValue}`, {
+        headers: {
+          "auth-token": `${accessToken}`,
+        },
+      })
+      .then((response) => {
+        history.push({
+          pathname: `/search/${searchValue}`,
+          state: {
+            searchResults: response.data,
+          },
+        });
+      })
+      .catch((error) => {
+        console.log(error.response);
+      });
+  };
 
   return (
     <div className="searchbar-container">
       <input
         type="text"
         placeholder="Find recipes"
-        class="searchbar-input"
-        value={searchValue}
-        onChange={(event) => setSearchValue(event.target.value)}
+        className="searchbar-input"
+        value={inputValue}
+        onChange={(event) => setInputValue(event.target.value)}
       ></input>
-      <button type="submit" class="searchbar-button">
+      <button
+        type="submit"
+        className="searchbar-button"
+        onClick={() => handleSearch(inputValue)}
+      >
         Search
       </button>
     </div>
   );
 };
 
-export default SearchBar;
+export default withRouter(SearchBar);
