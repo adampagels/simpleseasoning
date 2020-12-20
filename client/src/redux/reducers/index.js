c2eb3b2fbf50c7dffe32a6c6babed709550e157a
Adam Pagels <pagels@hotmail.ca>
2020-12-20 13:45:22 -0500

    Add fetchRecipes to combinedReducers

--- client/src/redux/reducers/index.js
+++ client/src/redux/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
 import fetchUsers from "../slices/user";
+import fetchRecipes from "../slices/recipe";
 
 export default combineReducers({
   fetchUsers,
+  fetchRecipes,
 });
