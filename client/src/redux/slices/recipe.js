ae5bae378943e66b9a566ce43281d4e6d9f62ba1
Adam Pagels <pagels@hotmail.ca>
2020-12-20 17:39:07 -0500

    Refactor code

--- client/src/redux/slices/recipe.js
+++ client/src/redux/slices/recipe.js
@@ -1,60 +1,43 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-export const initialState = {
-  loading: false,
-  hasErrors: false,
-  recipes: [],
-};
+export const fetchRecipes = createAsyncThunk(
+  "recipes/fetchRecipesStatus",
+  async (token, thunkAPI) => {
+    const response = await axios.get(
+      "http://localhost:5000/recipes/most-recent",
+      {
+        headers: {
+          "auth-token": token,
+        },
+      }
+    );
+    return response.data;
+  }
+);
 
 const recipeSlice = createSlice({
   name: "recipes",
-  initialState,
-  reducers: {
-    getRecipes: (state) => {
+  initialState: {
+    loading: false,
+    hasErrors: false,
+    recipes: [],
+  },
+  reducers: {},
+  extraReducers: {
+    [fetchRecipes.pending]: (state) => {
       state.loading = true;
     },
-    getRecipesSuccess: (state, { payload }) => {
+    [fetchRecipes.fulfilled]: (state, { payload }) => {
       state.recipes = payload;
       state.loading = false;
       state.hasErrors = false;
     },
-    getRecipesError: (state) => {
+    [fetchRecipes.rejected]: (state) => {
       state.loading = false;
       state.hasErrors = true;
     },
   },
 });
 
-export const {
-  getRecipes,
-  getRecipesSuccess,
-  getRecipesError,
-} = recipeSlice.actions;
-
 export default recipeSlice.reducer;
-
-export const fetchRecipes = createAsyncThunk(
-  "recipes/getRecipes",
-  async (token, thunkAPI) => {
-    // Set the loading state to true
-    thunkAPI.dispatch(getRecipes());
-    try {
-      const response = await axios.get(
-        "http://localhost:5000/recipes/most-recent",
-        {
-          headers: {
-            "auth-token": token,
-          },
-        }
-      );
-      const data = await response.data;
-      // Set the data
-      thunkAPI.dispatch(getRecipesSuccess(data));
-    } catch (error) {
-      // Set any errors while trying to fetch
-      thunkAPI.dispatch(getRecipesError());
-      console.log(error.response);
-    }
-  }
-);
