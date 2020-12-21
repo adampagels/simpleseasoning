8e7275b97615c2a91fd1bbd5f69b74d02d86126d
Adam Pagels <pagels@hotmail.ca>
2020-12-21 15:36:19 -0500

    Rename and refactor loginUser thunk to work for both registration and login

--- client/src/redux/slices/user.js
+++ client/src/redux/slices/user.js
@@ -1,13 +1,22 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-export const loginUser = createAsyncThunk(
-  "user/loginUserStatus",
+export const authenicateUser = createAsyncThunk(
+  "user/authenicateUserStatus",
   async (form, thunkAPI) => {
-    const response = await axios.post("http://localhost:5000/users/login", {
+    // If form contains username, then it is for registration
+    const authEndpoint = form.username ? "register" : "login";
+
+    const userLoginForm = { email: form.email, password: form.password };
+    const userRegisterForm = {
       email: form.email,
       password: form.password,
-    });
+      username: form.username,
+    };
+    const response = await axios.post(
+      `http://localhost:5000/users/${authEndpoint}`,
+      authEndpoint === "register" ? userRegisterForm : userLoginForm
+    );
     return response.data;
   }
 );
@@ -21,16 +30,16 @@ const userSlice = createSlice({
   },
   reducers: {},
   extraReducers: {
-    [loginUser.pending]: (state) => {
+    [authenicateUser.pending]: (state) => {
       state.loading = true;
     },
-    [loginUser.fulfilled]: (state, { payload }) => {
-      state.recipes = payload;
+    [authenicateUser.fulfilled]: (state, { payload }) => {
+      state.user = payload;
       state.loading = false;
       state.hasErrors = false;
       localStorage.setItem("auth-token", payload);
     },
-    [loginUser.rejected]: (state) => {
+    [authenicateUser.rejected]: (state) => {
       state.loading = false;
       state.hasErrors = true;
     },
