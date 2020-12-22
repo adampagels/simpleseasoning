1d2c97e02f24a9d276034d6b805eec0fa8639b0b
Adam Pagels <pagels@hotmail.ca>
2020-12-22 17:14:17 -0500

    Add fetchSingleRecipe to combinedReducers

--- client/src/redux/reducers/index.js
+++ client/src/redux/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import authenicateUser from "../slices/user/authenticateUser";
 import fetchRecipes from "../slices/recipe/recipe";
+import fetchSingleRecipe from "../slices/recipe/fetchSingleRecipe";
 
 export default combineReducers({
   authenicateUser,
   fetchRecipes,
+  fetchSingleRecipe,
 });
