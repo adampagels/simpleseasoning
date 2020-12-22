b2cd21ed24aba3d63a71f9dc8dedefd834642c26
Adam Pagels <pagels@hotmail.ca>
2020-12-22 17:11:36 -0500

    Add thunk and slice for getting single recipe

---
+++ client/src/redux/slices/recipe/fetchSingleRecipe.js
@@ -0,0 +1,44 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+export const fetchSingleRecipe = createAsyncThunk(
+  "singleRecipeFetch/fetchSingleRecipesStatus",
+  async (recipeId, thunkAPI) => {
+    const accessToken = localStorage.getItem("auth-token");
+    const response = await axios.get(
+      `http://localhost:5000/recipes/${recipeId}`,
+      {
+        headers: {
+          "auth-token": `${accessToken}`,
+        },
+      }
+    );
+    return response.data;
+  }
+);
+
+const fetchSingleRecipeSlice = createSlice({
+  name: "singleRecipeFetch",
+  initialState: {
+    loading: false,
+    hasErrors: false,
+    recipe: [],
+  },
+  reducers: {},
+  extraReducers: {
+    [fetchSingleRecipe.pending]: (state) => {
+      state.loading = true;
+    },
+    [fetchSingleRecipe.fulfilled]: (state, { payload }) => {
+      state.recipe = payload;
+      state.loading = false;
+      state.hasErrors = false;
+    },
+    [fetchSingleRecipe.rejected]: (state) => {
+      state.loading = false;
+      state.hasErrors = true;
+    },
+  },
+});
+
+export default fetchSingleRecipeSlice.reducer;
