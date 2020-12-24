6a51cb6b0e61c05083ba202491346673efe5f16a
Adam Pagels <pagels@hotmail.ca>
2020-12-24 11:06:35 -0500

    Add fetchUserId to combineReducers

--- client/src/redux/reducers/index.js
+++ client/src/redux/reducers/index.js
@@ -1,5 +1,6 @@
 import { combineReducers } from "redux";
 import authenicateUser from "../slices/user/authenticateUser";
+import fetchUserId from "../slices/user/fetchUserId";
 import fetchRecipes from "../slices/recipe/recipe";
 import fetchSingleRecipe from "../slices/recipe/fetchSingleRecipe";
 
@@ -7,4 +8,5 @@ export default combineReducers({
   authenicateUser,
   fetchRecipes,
   fetchSingleRecipe,
+  fetchUserId,
 });
