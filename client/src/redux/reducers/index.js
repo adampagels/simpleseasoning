5b2baecf35cf83013deccaf8b6ff84d0c67772b6
Adam Pagels <pagels@hotmail.ca>
2021-01-03 16:57:17 -0500

    Remove reducer

--- client/src/redux/reducers/index.js
+++ client/src/redux/reducers/index.js
@@ -1,6 +1,5 @@
 import { combineReducers } from "redux";
 import authenticateUser from "../slices/user/authenticateUser";
-import fetchUserId from "../slices/user/fetchUserId";
 import fetchRecipeList from "../slices/recipe/fetchRecipeList";
 import fetchSingleRecipe from "../slices/recipe/fetchSingleRecipe";
 import addNewRecipe from "../slices/recipe/addNewRecipe";
@@ -10,7 +9,6 @@ export default combineReducers({
   authenticateUser,
   fetchRecipeList,
   fetchSingleRecipe,
-  fetchUserId,
   fetchUserById,
   addNewRecipe,
 });
