61d983991ee32231aa56f87787d1d339dcd61ea6
Adam Pagels <pagels@hotmail.ca>
2021-01-28 13:53:59 -0500

    Add modal skeleton

---
+++ client/src/components/RatingModal/RatingModal.js
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import Modal from "react-modal";
+
+const customStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+  },
+  overlay: { zIndex: 1000 },
+};
+
+Modal.setAppElement("#root");
+
+const RatingModal = () => {
+  let subtitle;
+  const [modalIsOpen, setIsOpen] = useState(false);
+
+  const openModal = () => {
+    setIsOpen(true);
+  };
+
+  const afterOpenModal = () => {
+    subtitle.style.color = "#f00";
+  };
+
+  const closeModal = () => {
+    setIsOpen(false);
+  };
+
+  return (
+    <>
+      <button onClick={openModal}>Open Modal</button>
+      <Modal
+        isOpen={modalIsOpen}
+        onAfterOpen={afterOpenModal}
+        onRequestClose={closeModal}
+        style={customStyles}
+        contentLabel="Rating Modal"
+      >
+        <h2 ref={(_subtitle) => (subtitle = _subtitle)}>Hello</h2>
+        <button onClick={closeModal}>close</button>
+        <div>I am a modal</div>
+      </Modal>
+    </>
+  );
+};
+
+export default RatingModal;
