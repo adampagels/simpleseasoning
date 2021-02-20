f72d990f8861e08037be75f2c63089171aaa65b3
Adam Pagels <pagels@hotmail.ca>
2021-02-20 18:21:21 -0500

    Import and pass text prop to Header component

--- client/src/domain/SearchResults/SearchResults.js
+++ client/src/domain/SearchResults/SearchResults.js
@@ -1,9 +1,13 @@
 import React from "react";
 import RecipeCard from "../../components/RecipeCard/RecipeCard";
+import Header from "../../components/Header/Header";
 
 const SearchResults = ({ location }) => {
   return (
     <>
+      <Header
+        headerText={`${location.state.searchResults.length} results for '${location.state.inputValue}' `}
+      />
       <RecipeCard recipes={location.state.searchResults} />
     </>
   );
