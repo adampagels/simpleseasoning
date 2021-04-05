5d132b77bcdbe044cdd349f0ac80fa235ac02b6e
Adam Pagels <pagels@hotmail.ca>
2021-04-05 11:01:50 -0400

    Add animation via framer motion

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,5 +1,35 @@
 import React from "react";
+import { motion } from "framer-motion";
 
-const Header = ({ headerText }) => <h1 id="page-header">{headerText}</h1>;
+const Header = ({ headerText }) => {
+  const pageVariants = {
+    initial: {
+      opacity: 0,
+    },
+    in: {
+      opacity: 1,
+    },
+    out: {
+      opacity: 0,
+    },
+  };
+
+  const pageTransition = {
+    type: "spring",
+    ease: "anticipate",
+    duration: 0.6,
+  };
+  return (
+    <motion.div
+      initial="initial"
+      animate="in"
+      exit="out"
+      variants={pageVariants}
+      transition={pageTransition}
+    >
+      <h1 id="page-header">{headerText}</h1>{" "}
+    </motion.div>
+  );
+};
 
 export default Header;
