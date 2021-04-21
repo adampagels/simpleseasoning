d8f3b65f95b5f911d58ff77f0e6b9077500a6499
Adam Pagels <pagels@hotmail.ca>
2021-04-21 16:05:40 -0400

    Add hamburger menu with animation via framer-motion

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink, Redirect, useHistory } from "react-router-dom";
 import SearchBar from "../SearchBar/SearchBar";
 import { useSelector } from "react-redux";
+import { motion } from "framer-motion";
 
 const NavBar = () => {
   const accessToken = localStorage.getItem("auth-token");
@@ -10,7 +11,21 @@ const NavBar = () => {
     localStorage.removeItem("auth-token");
     history.push("/login");
   };
+  const [isOpen, setIsOpen] = useState(false);
   const { user } = useSelector((state) => state.authenticateUser);
+
+  const variants = {
+    open: {
+      opacity: 1,
+      x: 0,
+      width: "70%",
+      height: "100%",
+      borderRadius: 0,
+      right: 0,
+      bottom: 0,
+    },
+    closed: { opacity: 1 },
+  };
 
   return (
     <>
@@ -19,12 +34,16 @@ const NavBar = () => {
         <ul className="navbar-item-list">
           <div className="navbar-item-navlink-container">
             <li className="navbar-item">
-              <NavLink to="/" className="navbar-item-navlink">Home </NavLink>
+              <NavLink to="/" className="navbar-item-navlink">
+                Home{" "}
+              </NavLink>
             </li>
 
             <li className="navbar-item">
               {" "}
-              <NavLink to="/add-recipe" className="navbar-item-navlink">Add Recipe</NavLink>
+              <NavLink to="/add-recipe" className="navbar-item-navlink">
+                Add Recipe
+              </NavLink>
             </li>
 
             <li className="navbar-item">
@@ -46,6 +65,24 @@ const NavBar = () => {
           </li>
         </ul>
       </div>
+      <motion.nav
+        animate={isOpen ? "open" : "closed"}
+        variants={variants}
+        style={{
+          cursor: "pointer",
+          width: 100,
+          height: 100,
+          backgroundColor: "#d54217",
+          borderRadius: "100%",
+          position: "fixed",
+          zIndex: 20,
+          bottom: 20,
+          right: 20,
+        }}
+        transition={{ duration: 0.2 }}
+        onClick={() => setIsOpen((isOpen) => !isOpen)}
+        class="navbar-hamburgermenu"
+      ></motion.nav>
     </>
   );
 };
